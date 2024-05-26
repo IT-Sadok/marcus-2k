3cb4a56dd74e56a9727313cda36db0768ee5d7ed
Marcus <marcus.petrihen@gmail.com>
2024-05-26 08:23:24 +0300

    HW 1 - console store

---
+++ console-store/Clases/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_store.Clases
+{
+    internal class Product
+    {     
+        public required int id { get; set; }
+        public required string name { get; set; } = string.Empty;
+
+    }
+}
