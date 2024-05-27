adc5f70c4ca9f11c9a554c0d8ece61310741909e
Marcus <marcus.petrihen@gmail.com>
2024-05-27 08:36:06 +0300

    commit gitignore file & console-store-app files

---
+++ console-store/Program.cs
@@ -0,0 +1,55 @@
+﻿using ConsoleStore.Clases;
+
+Console.WriteLine("Hello, World!");
+
+List<Product> products = new List<Product>();
+
+var programWork = true;
+
+while(programWork)
+{ 
+    Console.WriteLine("Select action");
+    Console.WriteLine("1 - List of products");
+    Console.WriteLine("2 - Add product");
+    Console.WriteLine("Any button - Exit");
+
+    string inputKey = Console.ReadLine().ToString();
+
+   switch(inputKey)
+    {
+        case "1":
+            {
+                PrintListOfProducts();
+            } break;
+        case "2":
+            {
+                CreateNewProduct();
+            } break;
+        default:
+            {
+                programWork = false;
+            } break;
+    }
+
+    Console.WriteLine("==========================");
+}
+
+void PrintListOfProducts()
+{
+
+    foreach(Product product in products)
+    {
+        Console.WriteLine("Id: " + product.Id + " name: " + product.Name);
+    }
+
+    Console.WriteLine("Number of procuts " + products.Count);
+}
+
+void CreateNewProduct()
+{
+    Console.WriteLine("Enter name of product");
+
+    string nameOfProduct = Console.ReadLine();
+
+    products.Add(new Product { Id = products.Count + 1, Name = nameOfProduct });
+}
