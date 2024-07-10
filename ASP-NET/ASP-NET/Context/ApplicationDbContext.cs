b5e68cf187c80ac65706b706c9fbf6af63b47456
Marcus <marcus.petrihen@gmail.com>
2024-07-10 20:17:28 +0300

    init asp.net & create product entity & init migration db

---
+++ ASP-NET/ASP-NET/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASP_NET.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_NET.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
